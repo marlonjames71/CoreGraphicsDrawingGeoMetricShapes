e3d5a136554f8f8495ea0053a49a09a2b9a617b4
Marlon <marmoneymar@icloud.com>
2019-08-15 16:57:20 -0400

    First commit

---
+++ GeoShapeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CGDrawingGeoMetricShapes
+//
+//  Created by Marlon Raskin on 8/15/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import UIKit
+
+class GeoShapeViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		view.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+	}
+
+
+}
+
